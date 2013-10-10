1487c017806f67b94fd76c4c1b35d98fad15d141
jxdwinter <jxdwinter@gmail.com>
2013-10-10 11:24:36 +0800

    add the 3rd party libary
    1. cocoapods
    2. crashlytics
    3. reveal
    4. add -w to UIView+ImageSnapshot.m to avoid warning

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+pod "AFNetworking", "~> 2.0"
+pod 'RESideMenu', '~> 2.0.2'
+pod 'MBProgressHUD', '~> 0.8'
--- jxdribbble/jxdribbble_AppDelegate.m
+++ jxdribbble/jxdribbble_AppDelegate.m
@@ -7,11 +7,20 @@
 //
 
 #import "jxdribbble_AppDelegate.h"
+#import <Crashlytics/Crashlytics.h>
 
 @implementation jxdribbble_AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Crashlytics startWithAPIKey:@"adb6c6dba2e462026e3494de452a18ec98b17ee6"];
+    
+    
+    
+    
+    
+    
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
